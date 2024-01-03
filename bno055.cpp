fafe80edbd16abca1e910bc3fe0ccf873ad3222b
Aliakbar <ali.toghyan@gmail.com>
2024-01-03 00:05:51 -0800

    Read bno055 chip address.

--- bno055.cpp
+++ bno055.cpp
@@ -3,20 +3,32 @@
 #include <i2c.h>
 
 int main() {
-
-    if(i2c_open("/dev/i2c-0") == -1){
-        std::cerr << "Failed!" << std::endl;
+    int bus_fd = i2c_open("/dev/i2c-0");  // Open the I2C bus
+    if (bus_fd == -1) {
+        std::cerr << "Failed to open I2C bus!" << std::endl;
         return -1;
     }
 
     I2CDevice bno055;
     memset(&bno055, 0, sizeof(bno055));
-
     i2c_init_device(&bno055);
 
-    bno055.bus = 0;
-    bno055.addr = 0x28;
-    i2c_close(0);
+    bno055.bus = bus_fd;
+    bno055.addr = 0x28;   // Device address
 
+    unsigned int register_address = 0x00;  // Chip ID register address.
+    unsigned char buffer[1];               // Buffer to store the read data.
+    size_t length_to_read = sizeof(buffer);
+
+    ssize_t bytes_read = i2c_read(&bno055, register_address, buffer, length_to_read);
+    if (bytes_read < 0) {
+        std::cerr << "Failed to read from the I2C device" << std::endl;
+        i2c_close(bus_fd);  // Close the bus using the file descriptor
+        return -1;
+    }
+
+    printf("Chip ID: 0x%02X\n", buffer[0]);
+
+    i2c_close(bus_fd);
     return 0;
 }
